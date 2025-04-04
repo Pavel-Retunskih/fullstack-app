081d3f2f2ec512f8e9e37ecc921d14c470a11e0e
Pavel <pavelretunskih@gmail.com>
2025-04-04 10:27:16 +0300

    many fixes

--- client/src/app/model/store.ts
+++ client/src/app/model/store.ts
@@ -1,11 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
-import {appSlice} from "../slice/appSlice.ts";
 import {uploadPhotoApi} from "../../features/uploadPhoto/api/uploadPhotoApi.ts";
 import {userApi} from "../../entities/user/api/userApi.ts";
 
 export const store = configureStore({
   reducer: {
-    app: appSlice.reducer,
     [uploadPhotoApi.reducerPath]: uploadPhotoApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
   },
